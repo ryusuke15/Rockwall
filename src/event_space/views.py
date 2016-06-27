b13602290be49da42c8e2d41b024829ed65deeee
Ryusuke Lavalla <ryusukelavalla@gmail.com>
2016-06-27 14:55:59 +0900

    upda__te

--- src/event_space/views.py
+++ src/event_space/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import  messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import EmailMultiAlternatives
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -30,6 +32,21 @@ def event_home(request):
 
         instance = form.save(commit=False)
         instance.save()
+
+        form_email = form.cleaned_data.get("email")
+        form_name = form.cleaned_data.get("first_name")+" "+form.cleaned_data.get("last_name")
+        form_message = form.cleaned_data.get("message")
+        form_date = form.cleaned_data.get("date")
+
+        subject = 'Project Space Contact Form Notification'
+        from_email = settings.EMAIL_HOST_USER
+        body  = 'Name: %s<br/>Contact: %s<br/>Request Date: %s<br/>Message: %s<br/>'%(form_name, form_email, form_date, form_message)
+        to = 'ryusukelavalla@gmail.com'    
+        html_content = body
+        text_content = 'This is an example'
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
 
         messages.success(request, "Thank you very much. Your request has been submitted successfully." )
         return HttpResponseRedirect("/project_space") 
