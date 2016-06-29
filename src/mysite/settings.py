7f3e0478ec8fbcd9418bb0d491cb1fd240f10345
Ryusuke Lavalla <ryusukelavalla@gmail.com>
2016-06-29 00:49:54 +0000

    update

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!(60c504rrcvy*pdt9vzw-5%8#rmssnv7_#f6%jo!*g7lj6yjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["52.202.53.10","localhost", "www.yournewpage.online"]
 
