403cf66a4501e358d39325baab0255e1c79e8d18
Ryusuke Lavalla <ryusukelavalla@gmail.com>
2016-07-05 05:20:48 +0000

    final_update

--- src/coffee_shop/views.py
+++ src/coffee_shop/views.py
@@ -11,7 +11,7 @@ from .models import Blog, Contact, Mailing_list
 
 def coffee_shop(request):
     queryset_list = Blog.objects.order_by("-date")
-    paginator = Paginator(queryset_list, 5)
+    paginator = Paginator(queryset_list, 3)
     page = request.GET.get('page')
     try:
         queryset = paginator.page(page)
