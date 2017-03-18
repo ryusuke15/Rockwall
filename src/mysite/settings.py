327df0c915d9c69b9107a1eaac8ed47c3d58c95e
Ryusuke Lavalla <ryusukelavalla@gmail.com>
2017-03-18 06:48:39 +0000

    update

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Third Party App     
+    'clear_cache',
     'easy_thumbnails',
     'djangobower',
     'djangoseo',
